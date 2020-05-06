23b9ecb2a42ad388272b48bab102579f2bffec7e
AkopyaSha <akopyasha@bk.ru>
2020-05-06 22:12:15 +0300

    Дом.задания к первому уроку

--- src/lesson1/Task1_3.java
+++ src/lesson1/Task1_3.java
@@ -2,9 +2,7 @@ package lesson1;
 
 public class Task1_3 {
 
-    public double math (int a, int b, int c, int d, double e) {
-        e=a * (b + (c / d));
-        System.out.println(e);
-        return e;
+    public double math (double a, double b, double c, double d) {
+        return a * (b + (c / d));
     }
 }
